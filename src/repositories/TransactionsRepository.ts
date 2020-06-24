079f690a419f4f576ba2912a196ebe52c3b491a1
Rafaelltm <rafaelltm10@hotmail.com>
2020-06-24 12:14:29 -0300

    desafio_5

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,62 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
+
+  private balance: Balance = {
+    income: 0,
+    outcome: 0,
+    total: 0,
+  };
 
   constructor() {
     this.transactions = [];
   }
 
+  public isValidBalance(type: 'income' | 'outcome', value: number): boolean {
+    this.getBalance();
+
+    if (
+      (this.balance.total === 0 && type === 'outcome') ||
+      (type === 'outcome' && this.balance.total - value < 0)
+    ) {
+      return true;
+    }
+    return false;
+  }
+
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    if (type === 'income') {
+      this.balance.income += value;
+      this.balance.total += value;
+    } else {
+      this.balance.outcome += value;
+      this.balance.total -= value;
+    }
+
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
